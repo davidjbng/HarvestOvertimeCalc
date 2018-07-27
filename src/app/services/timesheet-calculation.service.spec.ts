369f790aca81aae5da012615d080d914b00e5e6e
David Jebing <david.jebing@lise.de>
2018-07-27 17:27:57 +0200

    added tests for timesheet calculation

--- src/app/services/timesheet-calculation.service.spec.ts
+++ src/app/services/timesheet-calculation.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TimesheetCalculation } from '@app/services/timesheet-calculation.service';
+import { TimeEntry } from '@app/models/timeEntry.model';
 
 describe('TimesheetCalculationServiceService', () => {
   beforeEach(() => {
@@ -11,5 +12,22 @@ describe('TimesheetCalculationServiceService', () => {
 
   it('should be created', inject([TimesheetCalculation], (service: TimesheetCalculation) => {
     expect(service).toBeTruthy();
+  }));
+  it('should return something', inject([TimesheetCalculation], (service: TimesheetCalculation) => {
+    expect(service.calculateResult(0, [], new Date(), new Date())).toBeDefined();
+  }));
+  it('amount of working days for one week should be 5', inject([TimesheetCalculation], (service: TimesheetCalculation) => {
+    expect(service.getAmountOfWorkingDays(new Date('2018-01-01'), new Date('2018-01-07'))).toEqual(5);
+  }));
+  it('given workedHours equals expectedHours overtime should be 0', inject([TimesheetCalculation], (service: TimesheetCalculation) => {
+    // Arrange
+    const workedHours: number = 40;
+    const timeEntries: TimeEntry[] = [{hours: workedHours}];
+
+    // Act
+    const result = service.calculateResult(workedHours, timeEntries, new Date('2018-01-01'), new Date('2018-01-07'));
+
+    // Assert
+    expect(result.overtime).toEqual(0);
   }));
 });
